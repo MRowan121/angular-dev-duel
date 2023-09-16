5e29cfc384bcf356081eac536de793a95feedfcf
Matt Rowan <matt.rowan121@gmail.com>
2023-09-15 20:56:27 -0400

    Add user content to duel page

--- client/src/app/duel/duel.component.ts
+++ client/src/app/duel/duel.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/user.service';
+import { userInfo } from '../app.component';
 
 @Component({
   selector: 'app-duel',
   templateUrl: './duel.component.html',
-  styleUrls: ['./duel.component.css']
+  styleUrls: ['./duel.component.css'],
 })
 export class DuelComponent implements OnInit {
-  usernameOne: string = ""
-  usernameTwo: string = ""
+  usernameOne: string = '';
+  usernameTwo: string = '';
+  user1: userInfo = {
+    username: '',
+    name: '',
+    location: '',
+    bio: '',
+    avatar_url: '',
+    titles: [],
+    'favorite-language': '',
+    'public-repos': 0,
+    'total-stars': 0,
+    'highest-starred': 0,
+    'perfect-repos': 0,
+    followers: 0,
+    following: 0,
+  };
+  user2: userInfo = {
+    username: '',
+    name: '',
+    location: '',
+    bio: '',
+    avatar_url: '',
+    titles: [],
+    'favorite-language': '',
+    'public-repos': 0,
+    'total-stars': 0,
+    'highest-starred': 0,
+    'perfect-repos': 0,
+    followers: 0,
+    following: 0,
+  };
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   receiveUsernameOne(valueEmitted: string) {
     this.usernameOne = valueEmitted;
@@ -23,7 +53,17 @@ export class DuelComponent implements OnInit {
     this.usernameTwo = valueEmitted;
   }
 
+  valid: boolean = false;
+
   onSubmit() {
-    this.userService.duelUsers(this.usernameOne, this.usernameTwo);
+    this.userService
+      .duelUsers(this.usernameOne, this.usernameTwo)
+      .then((response: any) => {
+        if (response[0].username && response[1].username) {
+          this.user1 = response[0];
+          this.user2 = response[1];
+          this.valid = true;
+        }
+      });
   }
 }
